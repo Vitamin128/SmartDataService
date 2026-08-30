daa827ed3d588f78074dccca3f8c512c772cc7bc
gan2729583308 <2729583308@qq.com>
2026-08-30 18:52:27 +0800

    common iteration

---
+++ src/main/java/com/example/smarttable/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.smarttable.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${test.message}")
+    private String message;
+
+    @GetMapping("/test/nacos")
+    public String testNacos() {
+        return message;
+    }
+}
