dcd5b714fb4b3fc787332263156549f67c201129
gan2729583308 <2729583308@qq.com>
2026-09-04 19:25:25 +0800

    common iteration

--- src/main/java/com/example/smarttable/controller/DeepseekChatDemo.java
+++ src/main/java/com/example/smarttable/controller/DeepseekChatDemo.java
@@ -2,20 +2,12 @@ package com.example.smarttable.controller;
 
 
 import com.example.smarttable.dao.User;
-import com.example.smarttable.dao.UserExcel;
-import com.example.smarttable.service.ChatService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.xmlbeans.impl.xb.xsdschema.Attribute;
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.print.attribute.standard.MediaSize;
 import java.awt.*;
 import java.io.IOException;
 
@@ -24,11 +16,6 @@ import java.io.IOException;
 @RequestMapping("/ai")
 @Validated
 public class DeepseekChatDemo {
-
-//    @Autowired
-//    private ChatService chatService;
-//    private ChatClient chatClient;
-
     @PostMapping(value = "/upload",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String chat(@RequestParam("file") MultipartFile file) throws IOException {
 //        chatService.readAsExcel(file);
